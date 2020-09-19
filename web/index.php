26c600219666209b989a0fe135b4a14a4784d56d
splucena <sumilangplucena@yahoo.com.ph>
2020-09-20 04:52:05 +0800

    added template.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
   $title = 'CS341: Web Backend Development II';
-
+  $currentDate = date("d.m.Y");
   //ob_start();
   include __DIR__ . '/template/template.php';
 ?>
--- web/template/template.php
+++ web/template/template.php
@@ -1,14 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=isset($title) ? $title : 'Home'?></title>
+    <link rel="stylesheet" href="../web/static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../web/static/css/style.css">
 </head>
+
 <body>
-    <header>Header goes here...</header>
-    <nav>Navigation goes here...</nav>
-    <main>Main goes here...</main>
-    <footer>Footer goes here</footer>
+    <div class="bg">
+        <nav class="navbar navbar-expand-lg navbar-custom">
+            <a class="navbar-brand" href="http://">CIT341</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    Web Backend Development II
+                </span>
+            </div>
+        </nav>
+        <main>
+            <div class="row">
+
+            </div>
+        </main>
+        <footer class="text-center p-2">
+            <div>
+            <span>&copy; Sumilang R. Plucena, All rights reserved</span><br />
+            <span>All images used are believed to be in "Fair Use". Please notify author if any are not and they will be
+                removed.</span><br />
+            <span>Last updated: <?=isset($currentDate) ? $currentDate : "2.2.2020" ?></span>
+            </div>            
+        </footer>
+    </div>
 </body>
+
 </html>
