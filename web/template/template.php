318ed9e48975ff29257d3367ad295c694d41f338
splucena <sumilangplucena@yahoo.com.ph>
2020-09-20 22:34:56 +0800

    added brand icon

--- web/template/template.php
+++ web/template/template.php
@@ -15,7 +15,7 @@
     <div class="container-fluid h-100">
         <div class="row">
             <nav class="navbar navbar-expand-lg w-100 navbar-custom">
-                <a class="navbar-brand" href="http://">CIT341</a>
+                <a class="navbar-brand" href="http://"><img class="brand-icon" src="./static/img/icon/coding.png" alt="Coding" /></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                     aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -32,44 +32,48 @@
                             <a class="nav-link" href="#">About</a>
                         </li>
                     </ul>
-                    <span class="navbar-text">
-                        <a href="">Web Backend Development II</a>
+                    <span class="navbar-text text-white">
+                        Web Backend Development II
                     </span>
                 </div>
             </nav>
         </div>
-        <div class="row h-75">
+        <div class="row">
             <main>
-                <div class="container-fluid text-center h-100">
-                    <div class="row content h-100">
-                        <div class="col-sm-1"></div>
-                        <div class="col-sm-5 text-left about">
+                <div class="container-fluid text-center">
+                    <div class="row content">
+                        <div class="col-sm-4 text-left about">
                             <div class="card special-card">
                                 <div class="card-header border-bottom">
                                     <div class="row">
-                                        <div class="col-sm-2"><img class="text-center" style="border-radius: 50%;" src="./static/img/splucena.jpg" width="80px" height="80px"
-                                        alt="My Image"></div>
-                                        <div class="col col-sm-10 p-3">
-                                            <h2 class="card-title author-title">Sumilang R. Plucena</h2>
+                                        <div class="col-sm-3"><img class="text-center" style="border-radius: 50%;"
+                                                src="./static/img/splucena.jpg" width="80px" height="80px"
+                                                alt="My Image"></div>
+                                        <div class="col col-sm-9 p-3">
+                                            <h4 class="card-title author-title">Sumilang R. Plucena</h4>
                                             <span class="author-title">Technology Enthusiast</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body border-bottom">                                    
+                                <div class="card-body border-bottom">
                                     <p class="card-text">
-                                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for
-                                        those
-                                        interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
-                                        Cicero are also reproduced in their exact original form, accompanied by English
-                                        versions from the 1914 translation by H. Rackham.
+                                        Experienced Information Technology Specialist with a demonstrated history of
+                                        working in the hospital & health care industry. Skilled in Leadership,
+                                        Automation, Web Frontend and Backend Development, Database Management,
+                                        Troubleshooting, and Linux. Proficient in Python, Java, PHP, JavaScript, CSS,
+                                        HTML, and SQL.
                                     </p>
                                     <a href="#" class="btn btn-success">PROJECTS</a>
                                 </div>
                                 <div class="card-footer text-muted">
                                     <div class="icon-container">
-                                        <span><a href="https://www.facebook.com/sumilang.plucena" target="_blank"><img class="icon" src="./static/img/icon/facebook.png" alt=""></a></span>
-                                        <span><a href="https://www.linkedin.com/in/sumilang-plucena-462a4a60/" target="_blank"><img class="icon" src="./static/img/icon/linkedin.png" alt=""></a></span>
-                                        <span><a href="https://github.com/splucena" target="_blank"><img class="icon" src="./static/img/icon/github.png" alt=""></a></span>
+                                        <span><a href="https://www.facebook.com/sumilang.plucena" target="_blank"><img
+                                                    class="icon" src="./static/img/icon/facebook.png" alt=""></a></span>
+                                        <span><a href="https://www.linkedin.com/in/sumilang-plucena-462a4a60/"
+                                                target="_blank"><img class="icon" src="./static/img/icon/linkedin.png"
+                                                    alt=""></a></span>
+                                        <span><a href="https://github.com/splucena" target="_blank"><img class="icon"
+                                                    src="./static/img/icon/github.png" alt=""></a></span>
                                     </div>
                                 </div>
                             </div>
@@ -81,7 +85,8 @@
             <footer class="w-100 text-right p-2">
                 <div>
                     <span>&copy; Sumilang R. Plucena, All rights reserved</span><br />
-                    <span>All images used are believed to be in "Fair Use".</span><br /><span>Please notify author if any are not and they
+                    <span>All images used are believed to be in "Fair Use".</span><br /><span>Please notify author if
+                        any are not and they
                         will be
                         removed.</span><br />
                     <span>Last updated: <?=isset($currentDate) ? $currentDate : "2.2.2020" ?></span>
