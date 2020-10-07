4554b5fa06c09f3c5a0fad31ae2e6f3cc3ee04c3
splucena <sumilangplucena@yahoo.com.ph>
2020-10-07 23:44:46 +0800

    removed db config reference

--- db/connection.php
+++ db/connection.php
@@ -14,8 +14,9 @@ $db["path"] = ltrim($db["path"], "/");
     . "port=5432;"
     . "sslmode=require;"
     . "password=3576d697b7cc2f959b57e89969b67c472da84fef00441f01213c1dfd45d41d66";
+$pdo = new PDO($dsn);
+*/
 
-$db = new PDO($dsn);*/
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s;sslmode=require;",
     $db["host"],
