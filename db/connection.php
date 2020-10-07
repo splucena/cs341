7539c22cf9429aa8d0f6ac1c91a2bc6917541459
splucena <sumilangplucena@yahoo.com.ph>
2020-10-07 23:45:55 +0800

    removed db config reference

--- db/connection.php
+++ db/connection.php
@@ -4,19 +4,6 @@
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
 
-//var_dump($db);
-//exit;
-
-/*$dsn = "pgsql:" 
-    . "host=ec2-54-224-175-142.compute-1.amazonaws.com;"
-    . "dbname=d37erhhggeh672;"
-    . "user=wzhaimlcbzqxqx;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=3576d697b7cc2f959b57e89969b67c472da84fef00441f01213c1dfd45d41d66";
-$pdo = new PDO($dsn);
-*/
-
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s;sslmode=require;",
     $db["host"],
