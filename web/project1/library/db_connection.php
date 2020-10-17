6317fe03b132872b3c820757689a3b9223490ab8
splucena <sumilangplucena@yahoo.com.ph>
2020-10-18 03:29:24 +0800

    return db

--- web/project1/library/db_connection.php
+++ web/project1/library/db_connection.php
@@ -37,6 +37,8 @@ function dbConnect() {
 
         $db =  new PDO("pgsql:host=$dbHost;port=$dbPort,dbname=$dbName", $dbUser, $dbPassword);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
     } catch (PDOException $ex) {
         echo 'Error!: ' . $ex->getMessage();
         die();
