cdd427a54f946bd4bc32b568a57e9e6424c5f576
splucena <sumilangplucena@yahoo.com.ph>
2020-10-18 05:11:37 +0800

    order detail form view

--- web/project1/main_content/order.html.php
+++ web/project1/main_content/order.html.php
@@ -15,7 +15,7 @@
     }
 
     // Search user
-    $html = "<div><form action='../controller/user.action.php' method='GET'>
+    $html = "<div><form action='../controller/order.action.php' method='GET'>
                 <div>
                     <table>
                         <tr>
@@ -35,7 +35,7 @@
                         <th>Total Amount</th>
                         <th>Shipping Date</th>
                         <th>Customer Name</th>
-                        <th>Processor Name</th>
+                        <th>Processed By</th>
                     </tr>
                 </thead>
                 <tbody>";
@@ -53,3 +53,50 @@
     }
     $html .= "</tbody></table></div>";
     echo $html;
+
+    $formOrder = "<div>
+        <h1>User Detail</h1>
+        <form method='POST' action='../controller/order.action.php'>
+            <ul>
+                <li>
+                    <label for='order_number'>Number</label>
+                    <input type='text' name='order_number' value='". ( isset($ordersById) ? $ordersById['order_number'] : '') . "' />
+                </li>
+                <li>
+                    <label for='order_desc'>Description</label>
+                    <input type='text' name='order_desc' value='". ( isset($ordersById) ? $ordersById['order_desc'] : '') . "' />
+                </li>
+                <li>
+                    <label for='status'>Status</label>
+                    <input type='text' name='status' value='". ( isset($ordersById) ? $ordersById['status'] : '') . "' />
+                </li>
+                <li>
+                    <label for='total_amount'>Total Amount</label>
+                    <input type='text' name='total_amount' value='". ( isset($ordersById) ? $ordersById['total_amount'] : '') . "' />
+                </li>
+                <li>
+                    <label for='shipping_date'>Shipping Date</label>
+                    <input type='text' name='shipping_date' value='". ( isset($ordersById) ? $ordersById['shipping_date'] : '') . "' />
+                </li>
+                <li>
+                    <label for='customer_name'>Customer Name</label>
+                    <input type='text' name='customer_name' value='". ( isset($ordersById) ? $ordersById['customer_name'] : '') . "'/>
+                </li>
+                <li>
+                    <label for='user_name'>Processed By</label>
+                    <input type='text' name='user_name' value='". ( isset($ordersById) ? $ordersById['user_name'] : '') . "'/>
+                </li>
+                <li>
+                    <div class='row'>
+                        <div class='col-50'>
+                            <input type='submit' name='action' value='Create'>
+                        </div>
+                        <div class='col-50'>
+                            <input type='submit' name='action' value='Update'>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </form>
+    </div>";
+    echo $formOrder;
