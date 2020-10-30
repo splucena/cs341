027f30d0ce9c7244c4f36548558e4c7315392e45
splucena <sumilangplucena@yahoo.com.ph>
2020-10-30 18:25:01 +0800

    updated user action

--- web/project1/controller/user.action.php
+++ web/project1/controller/user.action.php
@@ -15,30 +15,29 @@ if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
 }
 
-//echo $action;
-//exit;
-
 switch($action) {
     case 'Search':
         $display = 'search';
         //$searchTerm = filter_input(INPUT_GET, 'input-search', FILTER_SANITIZE_STRING);
-        $searchTerm = htmlspecialchars($_GET['input-search']);
+        //$searchTerm = htmlspecialchars($_GET['input-search']);
+        $searchTerm = filter_input(INPUT_GET, 'input-search', FILTER_SANITIZE_STRING);
         include('../view/user_detail.php');
 
         break;
     case 'PopulateForm':
         $display = 'populate-form';
-        $userId = htmlspecialchars($_GET['id']);
+        $userId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
         include('../view/user_detail.php');
         break;
     case 'Create':
-        $firstName = htmlspecialchars($_POST['fn']);
-        $lastName = htmlspecialchars($_POST['ln']);
-        $username = htmlspecialchars($_POST['username']);
-        $passwd = htmlspecialchars($_POST['passwd']);
-        $position = htmlspecialchars($_POST['position']);
-        $phone = htmlspecialchars($_POST['phone']);
+
+        $firstName = filter_input(INPUT_POST , 'fn', FILTER_SANITIZE_STRING);
+        $lastName = filter_input(INPUT_POST , 'ln', FILTER_SANITIZE_STRING);
+        $username = filter_input(INPUT_POST , 'username', FILTER_SANITIZE_STRING);
+        $passwd = filter_input(INPUT_POST , 'passwd', FILTER_SANITIZE_STRING);
+        $position = filter_input(INPUT_POST , 'position', FILTER_SANITIZE_STRING);
+        $phone = filter_input(INPUT_POST , 'phone', FILTER_SANITIZE_STRING);
 
         $user = new Users(null, $firstName, $lastName, $username, 
         $passwd, $position, $phone);
@@ -48,13 +47,13 @@ switch($action) {
         break;
     case 'Update':
 
-        $userId = (int)htmlspecialchars($_POST['user_id']);
-        $firstName = htmlspecialchars($_POST['fn']);
-        $lastName = htmlspecialchars($_POST['ln']);
-        $username = htmlspecialchars($_POST['username']);
-        $passwd = htmlspecialchars($_POST['passwd']);
-        $position = htmlspecialchars($_POST['position']);
-        $phone = htmlspecialchars($_POST['phone']);
+        $userId = (int)filter_input(INPUT_POST , 'user_id', FILTER_SANITIZE_NUMBER_INT);
+        $firstName = filter_input(INPUT_POST , 'fn', FILTER_SANITIZE_STRING);
+        $lastName = filter_input(INPUT_POST , 'ln', FILTER_SANITIZE_STRING);
+        $username = filter_input(INPUT_POST , 'username', FILTER_SANITIZE_STRING);
+        $passwd = filter_input(INPUT_POST , 'passwd', FILTER_SANITIZE_STRING);
+        $position = filter_input(INPUT_POST , 'position', FILTER_SANITIZE_STRING);
+        $phone = filter_input(INPUT_POST , 'phone', FILTER_SANITIZE_STRING);
 
         $user = new Users($userId, $firstName, $lastName, $username, 
         $passwd, $position, $phone);
@@ -63,7 +62,7 @@ switch($action) {
         break;
 
     case 'Deactivate':
-        $userId = (int)htmlspecialchars($_POST['user_id']);
+        $userId = (int)filter_input(INPUT_POST , 'user_id', FILTER_SANITIZE_NUMBER_INT);
 
         $user = new Users($userId);
         $user->deactivateUser($db);
